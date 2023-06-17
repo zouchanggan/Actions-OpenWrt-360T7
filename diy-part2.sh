46086e11d057bb56f0c68fef8ac317a9b64c3665
zouchanggan <61627503+zouchanggan@users.noreply.github.com>
2023-06-17 18:31:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,34 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
+
+# 删除软件包
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-vssr 
+# 删除软件依赖
+rm -rf feeds/packages/net/brook 
+rm -rf feeds/packages/net/chinadns-ng 
+rm -rf feeds/packages/net/hysteria 
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/shadowsocks-rust 
+rm -rf feeds/packages/net/tcping 
+rm -rf eeds/packages/net/trojan 
+rm -rf feeds/packages/net/trojan-go
+rm -rf feeds/packages/net/trojan-plus 
+rm -rf feeds/packages/net/v2raya
+rm -rf feeds/packages/net/v2ray-core 
+rm -rf feeds/packages/net/v2ray-geodata 
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/xray-core 
+rm -rf feeds/packages/net/xray-plugin
+rm -rf feeds/packages/net/mosdns
+
+#添加额外软件包
+git clone --depth=1 https://github.com/kenzok8/small.git package/small
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+./scripts/feeds update -a
+./scripts/feeds install -a
